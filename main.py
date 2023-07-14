518ac1dd1c590b8c4b51779e31f1458acb5aeef5
ElliottStrutton <76212111+ElliottStrutton@users.noreply.github.com>
2023-07-14 21:19:19 +0100

    intergrated files, added json templates

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from cryptography.fernet import Fernet
 import os, logging, json
 
+logging.basicConfig(filename='main.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(levelname)s : %(message)s')
+
+def key_gen():
+
+    if not os.path.isfile('key.key'):
+        key = Fernet.generate_key()
+        logging.info('Key Genarated')
+        logging.info(f'Key : {key}')
+
+        #print(f'Key : {key}')
+        with open('key.key','wb') as file:
+            file.write(key)
+            file.close()
+    else:
+        with open('key.key','r') as file:
+            key = file.read()
+            logging.info('Key Exists')
+            logging.info(f'key : {key}')
+            #print('Key Exists')
+
+
+
+
 if os.path.isfile('key.key'):
     with open('key.key','rb') as key_file:
         key = key_file.read()
         key_file.close()
     fernet = Fernet(key)
 else:
-    print('Please Genarate Key...')
+    print('No key found.')
+    if input('Would you like to generate a key now (y/N) : ').lower() == 'y':
+        key_gen()
 
 if os.path.isfile('passwords.json'):
     with open('passwords.json', 'rb') as pass_file:
@@ -16,4 +41,6 @@ if os.path.isfile('passwords.json'):
     pass_json = fernet.decrypt(pass_json_encr)
     pass_json = json.loads(pass_json)
 else:
-    print('Please Genarate Password File...')
+    print('No password file found.')
+    if input('Would you like to generate a password file now (y/N) : ').lower() == 'y':
+        #file_gen()
